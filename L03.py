cb83ee6bc215bd3350622e5586cb40d7275496ed
g0sie <malgorzataderkacz3@gmail.com>
2025-03-23 21:22:49 +0100

    add plot_stats_of_sort_func

--- L03.py
+++ L03.py
@@ -9,6 +9,8 @@ from L02 import (
     get_time_of_sort,
 )
 
+LENGTHS = [50, 100, 200, 500, 1000, 2000]
+
 
 def measure_time_n_times(n, lst_length):
     res = {"bubble": [], "insertion": [], "selection": []}
@@ -22,15 +24,15 @@ def measure_time_n_times(n, lst_length):
     return res
 
 
-def plot_mean_time_of_every_sort_func(lengths, times_of_sort):
+def plot_mean_time_of_every_sort_func(times_of_sort):
 
     bubble_mean = [mean(lst["bubble"]) for lst in times_of_sort]
     insertion_mean = [mean(lst["insertion"]) for lst in times_of_sort]
     selection_mean = [mean(lst["selection"]) for lst in times_of_sort]
 
-    plt.plot(lengths, bubble_mean, label="bubble sort")
-    plt.plot(lengths, insertion_mean, label="insertion sort")
-    plt.plot(lengths, selection_mean, label="selection sort")
+    plt.plot(LENGTHS, bubble_mean, label="bubble sort")
+    plt.plot(LENGTHS, insertion_mean, label="insertion sort")
+    plt.plot(LENGTHS, selection_mean, label="selection sort")
 
     plt.legend()
     plt.grid()
@@ -42,8 +44,30 @@ def plot_mean_time_of_every_sort_func(lengths, times_of_sort):
     plt.show()
 
 
+def plot_stats_of_sort_func(sort_func_key: str, times_of_sort):
+
+    max_time = [max(lst[sort_func_key]) for lst in times_of_sort]
+    mean_time = [mean(lst[sort_func_key]) for lst in times_of_sort]
+    min_time = [min(lst[sort_func_key]) for lst in times_of_sort]
+
+    plt.plot(LENGTHS, max_time, label="max")
+    plt.plot(LENGTHS, mean_time, label="avg")
+    plt.plot(LENGTHS, min_time, label="min")
+
+    plt.legend()
+    plt.grid()
+
+    plt.xlabel("długość listy")
+    plt.ylabel("czas sortowania [ns]")
+    plt.title(f"Czas wykonanania {sort_func_key} sort")
+
+    plt.show()
+
+
 if __name__ == "__main__":
 
-    lengths = [50, 100, 200, 500, 1000, 2000]
-    times_of_sort = [measure_time_n_times(10, length) for length in lengths]
-    plot_mean_time_of_every_sort_func(lengths, times_of_sort)
+    times_of_sort = [measure_time_n_times(100, length) for length in LENGTHS]
+    plot_mean_time_of_every_sort_func(times_of_sort)
+    plot_stats_of_sort_func("bubble", times_of_sort)
+    plot_stats_of_sort_func("insertion", times_of_sort)
+    plot_stats_of_sort_func("selection", times_of_sort)
